d74495f4707d24b71fa4e55f8bc537abbffd4005
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-13 23:26:40 +0300

    Creating quotes table

--- app/models.py
+++ app/models.py
@@ -1,14 +1,11 @@
-from os import name
+from . import db
 
-from flask.typing import TemplateFilterCallable
-
-
-class Quotes:
-    id
-    name
-    title 
-    message 
-    creator
+class Quotes(db.Model):
+    __tablename__ = "quotes"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    message = db.Column(db.String())
+    #creator
 
 class Comment:
     id
