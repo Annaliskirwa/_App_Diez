2b2b1d4a56bb954a5340517bc28f51b833af9af0
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-14 00:57:05 +0300

    crud sqlalchemy

--- manage.py
+++ manage.py
@@ -7,5 +7,9 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 
+@manager.shell
+def make_shell_context():
+  return dict(app=app, db=db, Quotes=Quotes)
+
 if __name__ == '__main__':
     manager.run()
