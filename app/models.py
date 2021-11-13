440cead4576a67602a6545c8e4f9c9231cd442d2
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-13 23:29:17 +0300

    Creating comment table

--- app/models.py
+++ app/models.py
@@ -7,10 +7,11 @@ class Quotes(db.Model):
     message = db.Column(db.String())
     #creator
 
-class Comment:
-    id
-    comment 
-    commentor 
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key = True)
+    comment = db.Column(db.String())
+    #commentor 
 
 class User:
     id
