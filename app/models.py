1779eaa2b0addf4ff52c3b4dce8bf122da53f9a1
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-14 01:32:51 +0300

    Creating db relationship, adding and commiting sessions

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import backref
 from . import db
 
 class Quotes(db.Model):
@@ -5,18 +6,22 @@ class Quotes(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(255))
     message = db.Column(db.String())
-    #creator
+    creator = db.relationship('User', backref='quotes', lazy='dynamic')
 
     def __repr__(self):
         return f'Quotes {self.title}'
 
 class Comment(db.Model):
-    __tablename__ = "usercomments"
+    __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key = True)
     comment = db.Column(db.String())
     #commentor 
 
 class User(db.Model):
-    __tablename__ = "comments"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255))
+    quotes = db.relationship('Quotes', backref='users', lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.name}'
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import Quotes
+from app.models import Quotes, User, Comment
 from flask_script import Manager, Server
 
 app = create_app('development')
@@ -9,7 +9,7 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-  return dict(app=app, db=db, Quotes=Quotes)
+  return dict(app=app, db=db, Quotes=Quotes, User=User, Comment=Comment)
 
 if __name__ == '__main__':
     manager.run()
